fea93eab504d563d6db5ea54bbfeac1e567992f7
Vivekanandan Balasubramanian <vivekb@titan-ext1.ccs.ornl.gov>
2017-09-22 18:23:50 -0400

    adding gromacs data with gromacs/5.1.0 on titan

--- gromacs-benchmark/data-ntl9/extract_time.py
+++ gromacs-benchmark/data-ntl9/extract_time.py
@@ -4,13 +4,13 @@ from glob import glob
 
 if __name__ == '__main__':
 
-    logs = glob('threads_*/md.log')
+    logs = glob('threads_*/gromacs-*')
     prof = open('profile.txt','w')
     prof.write('Threads/Cores Walltime\n')
     for log in logs:
         threads = log.split('/')[0].strip().split('_')[1].strip()
         with open(log,'r') as fp:
-            line = fp.readlines()[-4:-3][0].strip().split(' ')
+            line = fp.readlines()[-7:-6][0].strip().split(' ')
             line = filter(lambda a: a != "", line)
             walltime = line[2]
             print 'Threads/Cores: ',threads,'Walltime: ', walltime
