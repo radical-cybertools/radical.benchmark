6471850a94dccc6350b54c7f1962012338ccffd4
Vivekanandan Balasubramanian <vivekb@titan-ext7.ccs.ornl.gov>
2017-09-22 20:47:28 -0400

    adding profile with openmpi gromacs

---
+++ gromacs-benchmark/openmpi_gromacs_mpi/data-bpti/threads_4/cleanup.sh
@@ -0,0 +1,1 @@
+rm ener.edr md.log mdout.mdp topol.tpr \#* gromacs-bpti* confout.gro state* md.cpt  md.edr  md.gro  md.tpr  md.xtc -rf
---
+++ gromacs-benchmark/openmpi_gromacs_mpi/data-ntl9/threads_16/orig.sh
@@ -0,0 +1,2 @@
+gmx_mpi grompp -n index.ndx -f grompp.mdp -c input.gro -maxwarn 1
+#gmx_mpi mdrun
